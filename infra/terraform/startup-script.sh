c215d5e7ae9e5265e25b96888414696f0db7d515
Karthikeyan suresh <karthis96@gmail.com>
2024-01-12 07:06:19 -0500

    testing artifcat registry after adding permissions

--- infra/terraform/startup-script.sh
+++ infra/terraform/startup-script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 sudo apt-get update
 sudo apt-get install -y docker.io
-sudo docker run -d -p 8080:8080 gcr.io/${project_id}/hello-world:${image_tag}
+sudo docker run -d -p 8080:8080 us-east1-docker.pkg.dev/${project_id}/hello-world:${image_tag}
