193a7dfabb37c61a1576575a8ea26168c60d1516
Karthikeyan suresh <karthis96@gmail.com>
2024-01-17 10:07:36 -0500

    t

--- infra/terraform/prod/main.tf
+++ infra/terraform/prod/main.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "prod_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "centos-cloud/centos-7"
     }
   }
 
