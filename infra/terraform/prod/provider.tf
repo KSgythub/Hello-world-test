f778ba7a6ab64a9d07c10d2f97bab43d9885c963
Karthikeyan suresh <karthis96@gmail.com>
2024-01-17 07:17:42 -0500

    Updating required-providers

--- infra/terraform/prod/provider.tf
+++ infra/terraform/prod/provider.tf
@@ -10,3 +10,13 @@ provider "google" {
   project = var.gcp_project
   region = var.gcp_region
 }
+
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.12.0"
+    }
+  }
+}
